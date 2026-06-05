227c485fabe74c02cbf8f66af14ba478c536f5b5
Johan <johannn081023@gmail.com>
2026-06-05 09:36:35 -0500

    Fix GitHub Pages static output

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
-    outDir: 'dist',
+    outDir: '.',
     assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
